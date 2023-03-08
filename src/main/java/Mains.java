3c5ad17dea0c08fa2e2e71baa86eb00479873d1c
깃osoreee3 <taecoo3@naver.com>
2023-03-08 12:25:49 +0900

    javafir

--- src/main/java/Mains.java
+++ src/main/java/Mains.java
@@ -1,2 +1,12 @@
 public class Mains {
+    public static void main(String[] args) {
+        int age;
+        age = 50;
+        System.out.println("당신의 나이 : " + age);
+        if (age >= 20 && age <= 34) {
+            System.out.println("청년입니다.");
+        } else {
+            System.out.println("청년이 아닙니다..");
+        }
+    }
 }
